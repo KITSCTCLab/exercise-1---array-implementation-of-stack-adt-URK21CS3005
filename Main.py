067c604b278b96e87d69cf099cd37cf841549b60
Joshua Thomas K <109951862+URK21CS3005@users.noreply.github.com>
2022-07-25 15:08:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,18 +5,23 @@ class Stack:
         self.size = size
 
     def is_empty(self):
-        # Write code here
+        self.top=-1
+        return self.top==-1
 
     def is_full(self):
-        # Write code here
+        return self.top==self.size-1
 
     def push(self, data):
         if not self.is_full():
-            # Write code here
+            self.top+=1
+            x=int(input("Enter the data : "))
+            self.items[self.top]=x
 
     def pop(self):
         if not self.is_empty():
-            # Write code here
+            print("The deleted element is : ",self.items[self.top])
+            self.items[self.top]=None
+            self.top-=1
 
     def status(self):
         # Write code here
